6b6bd49d98febf001aae6ab75b394c5be4df7141
Jason Springer <jsnsprngr@gmail.com>
2019-04-29 19:50:32 -0400

    Edit config settings for Auth0

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const DIST_DIR = path.join(__dirname, './dist');
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+app.use(express.static(DIST_DIR));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'./dist/index.html'));
 });
