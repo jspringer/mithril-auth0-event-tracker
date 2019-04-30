62648c1090371c4469b8dad1b388ebb0cde60d8f
Jason Springer <jsnsprngr@gmail.com>
2019-04-30 18:22:01 -0400

    Edit config settings for Auth0

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,17 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+require('dotenv').config({ path: path.resolve(process.cwd(), '.env.production') });
+require('dotenv').config();
+const webpack = require('webpack');
+
+// inject envs
+let plugins = [];
+let envs = {};
+Object.keys(process.env).filter(key => key.startsWith('MITHRIL_')).forEach(key => {
+  envs[key] = JSON.stringify(process.env[key]);
+});
+plugins.push(new webpack.DefinePlugin(envs));
 
 module.exports = {
   entry: {
@@ -8,7 +19,7 @@ module.exports = {
   },
   plugins: [
     // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
-    new CleanWebpackPlugin(),
+		new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "./index.html",
 			filename: "index.html",
