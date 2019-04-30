1f8396e3f1e34e58ddcc065d6440b4fe858b5b62
Jason Springer <jsnsprngr@gmail.com>
2019-04-30 00:27:16 -0400

    Edit config settings for Auth0

--- server.js
+++ server.js
@@ -4,12 +4,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 const DIST_DIR = path.join(__dirname, './dist');
 
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.use(express.static(DIST_DIR));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'./dist/index.html'));
+app.get('/*', function(req,res) {  
+  res.sendFile(path.join(__dirname,'./dist/index.html'));
 });
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/services/auth.js
+++ src/services/auth.js
@@ -5,10 +5,10 @@ import auth0 from "auth0-js";
 
 export default class Auth {
   auth0 = new auth0.WebAuth({
-    domain: process.env.AUTH0_DATA.DOMAIN || AUTH0_DATA.DOMAIN,
-    clientID: process.env.A0_DATA.CLIENT_ID || AUTH0_DATA.CLIENTID,
-    redirectUri: process.env.AUTH0_DATA.CALLBACK_URL || AUTH0_DATA.CALLBACKURL,
-    audience: process.env.A0_DATA.AUDIENCE || AUTH0_DATA.AUDIENCE,
+    domain: process.env.A0_DOMAIN || AUTH0_DATA.DOMAIN,
+    clientID: process.env.A0_CLIENT_ID || AUTH0_DATA.CLIENTID,
+    redirectUri: process.env.AUTH0_CALLBACK_URL || AUTH0_DATA.CALLBACKURL,
+    audience: process.env.A0_AUDIENCE || AUTH0_DATA.AUDIENCE,
     responseType: "token id_token",
     scope: "openid profile email"
   });
