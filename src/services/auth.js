087db73afdd7964cfbe8676b8335083be009dec5
Jason Springer <jsnsprngr@gmail.com>
2019-04-30 12:56:49 -0400

    Edit config settings for Auth0

--- src/services/auth.js
+++ src/services/auth.js
@@ -14,8 +14,8 @@ import auth0 from "auth0-js";
 
 export default class Auth {
   auth0 = new auth0.WebAuth({
-    domain: process.env.A0_DOMAIN,
     clientID: process.env.A0_CLIENT_ID,
+    domain: process.env.A0_DOMAIN,
     redirectUri: process.env.AUTH0_CALLBACK_URL,
     audience: process.env.A0_AUDIENCE,
     responseType: "token id_token",
